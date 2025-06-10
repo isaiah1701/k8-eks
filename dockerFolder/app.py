15e448e6013c38994c21344f606c5f0931cb5e41
isaiah1701 <imichael1403@gmail.com>
2025-06-10 20:54:36 +0100

    trigger deployment with latest image

--- dockerFolder/app.py
+++ dockerFolder/app.py
@@ -40,3 +40,4 @@ def architecture_page():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)# Updated Tue Jun 10 20:47:44 BST 2025
+# Updated Tue Jun 10 20:54:36 BST 2025
